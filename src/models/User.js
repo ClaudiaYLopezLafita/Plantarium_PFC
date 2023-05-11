cee8c25f75fb88388e4522ae627d05562ee3c784
ClaudiaYLopezLafita <claudiayll94@gmail.com>
2023-05-11 18:46:05 +0200

    modificando generador de codSubscription

--- src/models/User.js
+++ src/models/User.js
@@ -66,7 +66,6 @@ var UserSchema = new Schema({
     subscription: {
         type: Schema.Types.String,
         ref: 'Subscription',
-        default: null
     }
 })
 
--- src/routes/subscriptions.js
+++ src/routes/subscriptions.js
@@ -21,10 +21,10 @@ router.get('/',function(req, res, next) {
 router.post('/', async (req, res) =>{
     const {date, type, pays, garden, user} = req.body;
     console.log(req.body)
-    const codSubscription = generateRandomNumSubscription();
     try {
         const userinfo = await User.findOne({ username: user }).populate('subscription');
         if (userinfo) {
+            const codSubscription = generateRandomNumSubscription(user);
             const newSubscriptions = await Subscription.create({
                 codSubscription,
                 date,
@@ -35,7 +35,7 @@ router.post('/', async (req, res) =>{
             });
             userinfo.subscription = newSubscriptions;
             userinfo.save();
-            res.status(200).send("Suscripcion  realizada correctamente");
+            res.status(200).send("Suscripcion realizada correctamente");
         } else {
             res.status(500).send("Usuario no localizado");
         }
@@ -45,14 +45,18 @@ router.post('/', async (req, res) =>{
     }
 })
 
-function generateRandomNumSubscription() {
-    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+function generateRandomNumSubscription(username) {
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
     let code = '';
+    // Obtener los tres primeros caracteres del nombre de usuario
+    const truncatedUsername = username.substring(0, 3).toUpperCase(); 
     for (let i = 0; i < 8; i++) {
         const randomIndex = Math.floor(Math.random() * characters.length);
         code += characters.charAt(randomIndex);
     }
-    return code;
+    // Agregar los caracteres generados al nombre de usuario truncado
+    const result = `${truncatedUsername}-${code}`
+    return result; 
 }
 
 module.exports = router;
